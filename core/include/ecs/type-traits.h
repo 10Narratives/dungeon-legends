704a522918567ca25597dfa8f35e81ed42cd7981
10Narratives <vanyatertyshnikov@yandex.ru>
2024-11-15 18:59:14 +0300

    Add first version of component type trait

---
+++ core/include/ecs/type-traits.h
@@ -0,0 +1,14 @@
+#ifndef CORE_ECS_TYPE_TRAITS_H
+#define CORE_ECS_TYPE_TRAITS_H
+
+#include <concepts>
+#include "component-base.h"
+
+namespace core::ecs {
+
+template <typename T>
+concept is_component = std::derived_from<T, ComponentBase>;
+
+}
+
+#endif
