6b1141481ca375376a9db21921f0f61d1fefb9d2
10Narratives <vanyatertyshnikov@yandex.ru>
2025-05-28 18:54:45 +0300

    Add start project layout

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+int main() {
+    try {
+
+    } catch (const std::exception& fatal_error) {
+        std::cerr << fatal_error.what() << "\n";
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
